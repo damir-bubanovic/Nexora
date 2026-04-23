6ac7ad668e9d1a39aeb1339c1d06cc373142e7f4
Damir Bubanovic <damir.bubanovic@yahoo.com>
2026-04-23 04:08:42 +0200

    fix: resolve Tailwind asset setup for Breeze UI styling

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -65,7 +65,7 @@
 
                         <div class="flex justify-end">
                             <button type="submit"
-                                    class="bg-blue-600 text-black px-4 py-2 rounded hover:bg-blue-700">
+                                class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
                                 Save Project
                             </button>
                         </div>
