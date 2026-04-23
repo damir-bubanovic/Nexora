74b4a9af4f3373bd37dede0f12132bc8a25e307b
Damir Bubanovic <damir.bubanovic@yahoo.com>
2026-04-23 05:26:20 +0200

    feat: add tasks table and relationships to projects

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+        'created_by',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
